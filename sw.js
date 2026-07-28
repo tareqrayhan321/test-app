ebb077b979d78a6f0c7c594be174ab1f91f1f948
tareqrayhan321 <tareqrayhan1@gmail.com>
2026-07-28 09:32:35 +0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    PWA অফলাইন সাপোর্টের জন্য
 ══════════════════════════════════════════ */
 
-const CACHE_NAME = 'imran-cloth-store-v14';
+const CACHE_NAME = 'imran-cloth-store-v15';
 
 /* যেসব ফাইল ইনস্টলেশনের সময়ই ক্যাশ হবে */
 const PRECACHE_URLS = [
